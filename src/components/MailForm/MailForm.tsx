5b0bc291a92e728e5438478aede809f13b8f1faf
Akky-Y <chilcolor.1216a@gmail.com>
2024-04-28 17:49:14 +0900

    ✨ お問い合わせフォーム作成

---
+++ src/components/MailForm/MailForm.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { useMailForm } from "@/hooks/useMailForm";
+import { useEffect } from "react";
+import { ClipLoader } from "react-spinners";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function MailForm() {
+  const { form, onSubmit } = useMailForm();
+
+  useEffect(() => {
+    if (form.formState.isSubmitSuccessful) {
+      toast.success("メール送信に成功しました！");
+    }
+  }, [form.formState.isSubmitSuccessful]);
+
+  return (
+    <Form {...form}>
+      <ToastContainer />
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="container flex flex-col gap-3"
+      >
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>ユーザー名</FormLabel>
+              <FormControl>
+                <Input placeholder="ユーザー名" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>メールアドレス</FormLabel>
+              <FormControl>
+                <Input placeholder="メールアドレス" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="subject"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>主題</FormLabel>
+              <FormControl>
+                <Input placeholder="主題" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>本文</FormLabel>
+              <FormControl>
+                <Textarea placeholder="本文" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="file"
+          render={({ field: { value, onChange, ...fieldProps } }) => (
+            <FormItem>
+              <FormLabel>ファイル</FormLabel>
+              <FormControl>
+                <Input
+                  accept="image/"
+                  type="file"
+                  onChange={(event) => {
+                    onChange(event.target.files);
+                  }}
+                  {...fieldProps}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" disabled={form.formState.isSubmitting}>
+          {form.formState.isSubmitting ? <ClipLoader /> : "Submit"}
+        </Button>
+      </form>
+    </Form>
+  );
+}
---
+++ src/components/email-template.tsx
@@ -0,0 +1,19 @@
+type EmailTemplateProps = {
+  username: string;
+  email: string;
+  content: string;
+};
+
+export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
+  username,
+  email,
+  content,
+}) => {
+  return (
+    <div>
+      <h1>こんにちは、{username}です。</h1>
+      <p>{email}から届きました。</p>
+      <p>{content}</p>
+    </div>
+  );
+};
